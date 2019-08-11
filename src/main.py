8154a7186688e8e528c8173690999223b5f38645
myithelper <714525329@qq.com>
2019-08-11 11:05:28 +0900

    3

--- src/main.py
+++ src/main.py
@@ -1,53 +1,76 @@
 def main():
     print('tt')
+    space = '	'
 
     with open ('../word/template.html') as f:
         t=f.read()
         print(t)
 
 
-    with open('dictionary.csv',encoding="shift-jis") as f:
+    with open('dictionary.txt',encoding="utf-16") as f:
         s = f.read()
         print(s)
     ss =s.split('\n')
     words = []
+    infos=[]
     for i in range(len(ss)):
         line = ss[i]
-        parts = line.split(',')
-        if not parts[0]:
-            continue
+        parts = line.split(space)
+        #parts = line.split('	')
+
+        
         if parts[0]=='index':
             continue
-        word = parts[1]
-        words.append(word)
-        pro = parts[3]
+
+        pro = parts[4]
         if not pro:
-            break
-        meaning =parts[4]
-        meaning=addLink(meaning,words)
-        if i<len(ss)-1:
-            word_next=ss[i+1].split(',')[1]
-            createHtml(t,word,pro,meaning,word_next)
-        else:
-            createHtml(t,word,pro,meaning,'')
+            continue
+        word = parts[2]
+        words.append(word)
+        meaning =parts[5]
+        infos.append([word,pro,meaning])
 
+
+
+    words.reverse()        
+    print('aaaaaaaaaaaaaaaaaaaaa')
     print(words)
+    print('bbbbbbbbbbbbbbbbbbb')
+    print(infos)
+    for i in range(len(infos)):
+        print(i)
+        if i <len(infos)-1:
+
+            createHtml(t,infos[i][0],infos[i][1],infos[i][2],words,infos[i+1][0])
+        else:
+
+            createHtml(t,infos[i][0],infos[i][1],infos[i][2],words,'')
 
 
 
-    
-def addLink(meaning,words):
-    for word in words:
-        if word in meaning:
-            link='<a href={word}.html>{word}</a>'.format(word=word)
-            meaning=meaning.replace(word,link)
-    return meaning
+
+               
+def addLink(word,words):
+    done=[]
+    for w in words:
+        if w in word and w!=word:
+            flag=False
+            for d in done:
+                if w in d:
+                    flag=True
+            if flag:
+                continue
+            link='<a href={w}.html>{w}</a>'.format(w=w)
+            word=word.replace(w,link)
+            done.append(w)
+    return word
 
 
 
 
-def createHtml(t,word,pro,meaning,word_next):
+def createHtml(t,word,pro,meaning,words,word_next):
     with open ('../word/{word}.html'.format(word=word),'w') as f:
+        word=addLink(word,words)
         html = t.replace('{word}',word).replace('{pro}',pro).replace('{meaning}',meaning)
         if word_next:
             html = html.replace('{word_next}',word_next)
